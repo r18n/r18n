3405124fc1e1926607f0019762f20d21aa6cc17a
Andrey A.I. Sitnik <andrey@sitnik.ru>
2008-09-21 14:11:28 +0400

    Add methods to I18n to use Translation and Locale

--- lib/r18n/i18n.rb
+++ lib/r18n/i18n.rb
@@ -18,7 +18,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
 module R18n
-  # General class to i18n support in your application.
+  # General class to i18n support in your application. It load Translation and
+  # Locale classes and create pretty way to use it.
+  #
+  # To get translation you can use same with Translation way – use method with
+  # translation’s name or <tt>[name]</tt> method. Translations will be also
+  # loaded for default locale, +sublocales+ from first in +locales+ and general
+  # languages for dialects (it will load +fr+ for +fr_CA+ too).
+  #
+  # See Translation and Locale documentation.
+  #
+  # == Usage
+  # translations/ru.yml
+  #
+  #   one: Один
+  #
+  # translations/en.yml
+  #
+  #   one: One
+  #   two: Two
+  #
+  # example.rb
+  # 
+  #   i18n = R18n::I18n.new(['ru', 'en'], 'translations/')
+  #   
+  #   i18n.one   #=> "Один"
+  #   i18n.two   #=> "Two"
+  #   
+  #   i18n.locale['title']     #=> "Русский"
+  #   i18n.locale['code']      #=> "ru"
+  #   i18n.locale['direction'] #=> "ltr"
+  #   
+  #   i18n.locales.first['title'] #=> "Русский"
+  #   i18n.locales.last['title']  #=> "English"
   class I18n
     @@default = 'en'
 
@@ -47,6 +79,84 @@ module R18n
       end
       locales.sort! { |a, b| b[1] <=> a[1] }
       locales.map! { |i| i[0] }
+    end
+    
+    # User locales, ordered by priority
+    attr_reader :locales
+    
+    # Dir with translations files
+    attr_reader :translations_dir
+    
+    # First locale with locale file
+    attr_reader :locale
+    
+    # Create i18n for +locales+ with translations from +translations_dir+ and
+    # locales data. Translations will be also loaded for default locale,
+    # +sublocales+ from first in +locales+ and general languages for dialects
+    # (it will load +fr+ for +fr_CA+ too).
+    #
+    # +Locales+ must be a locale code (RFC 3066) or array, ordered by priority.
+    def initialize(locales, translations_dir)
+      locales = locales.to_a if String == locales.class
+      
+      @locales = locales.map do |locale|
+        if Locale.exists? locale
+          Locale.new locale
+        else
+          locale
+        end
+      end
+      
+      locales << @@default
+      if Locale == @locales.first.class
+        locales += @locales.first['sublocales']
+      end
+      locales.each_with_index do |locale, i|
+        if "_" == locale[2..2]
+          locales.insert(i + 1, locale[0..1])
+        end
+      end
+      locales.uniq!
+      
+      locales.each do |locale|
+        if Locale.exists? locale
+          @locale = Locale.new(locale)
+          break
+        end
+      end
+      
+      @translations_dir = File.expand_path(translations_dir)
+      @translation = Translation.load(locales, @translations_dir)
+    end
+    
+    # Return titles (or code for translation without locale file) of available
+    # translations.
+    def translations
+      Translation.available(@translations_dir).map do |locale|
+        if Locale.exists? locale
+          Locale.new(locale)['title']
+        else
+          locale
+        end
+      end
+    end
+    
+    # Short and pretty way to get translation by method name. If translation
+    # has name like object methods (+new+, +to_s+, +methods+) use <tt>[]</tt>
+    # method to access.
+    #
+    # Translation can contain variable part. Just set is as <tt>%1</tt>,
+    # <tt>%2</tt>, etc in translations file and set values as methods params.
+    def method_missing(name, *params)
+      self[name.to_s, *params]
+    end
+    
+    # Return translation with special +name+.
+    #
+    # Translation can contain variable part. Just set is as <tt>%1</tt>,
+    # <tt>%2</tt>, etc in translations file and set values in next +params+.
+    def [](name, *params)
+      @translation[name, *params]
     end
   end
 end
--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe R18n::I18n do
+  DIR = Pathname(__FILE__).dirname + 'translations/general'
 
   it "should parse HTTP_ACCEPT_LANGUAGE" do
     R18n::I18n.parse_http_accept_language(nil).should == []
@@ -12,6 +13,51 @@ describe R18n::I18n do
   it "should has default locale" do
     R18n::I18n.default = 'ru'
     R18n::I18n.default.should == 'ru'
+  end
+
+  it "should load locales" do
+    i18n = R18n::I18n.new('en', DIR)
+    i18n.locales.should == [R18n::Locale.new('en')]
+  
+    i18n = R18n::I18n.new(['ru', 'no_LC'], DIR)
+    i18n.locales.should == [R18n::Locale.new('ru'), 'no_LC']
+  end
+
+  it "should return translations dir" do
+    i18n = R18n::I18n.new('en', DIR)
+    i18n.translations_dir.should == DIR.expand_path.to_s
+  end
+
+  it "should load translations" do
+    i18n = R18n::I18n.new(['ru', 'en'], DIR)
+    i18n.one.should == 'Один'
+    i18n.only.english.should == 'Only in English'
+  end
+
+  it "should load default translation" do
+    R18n::I18n.default = 'en'
+    
+    i18n = R18n::I18n.new('no_LC', DIR)
+    i18n.one.should == 'ONE'
+    i18n.two.should == 'Two'
+  end
+  
+  it "should load sublocales for first locale" do
+    R18n::I18n.default = 'no_TR'
+    
+    i18n = R18n::I18n.new('ru', DIR)
+    i18n.one.should == 'Один'
+    i18n.two.should == 'Two'
+  end
+  
+  it "should return available translations" do
+    i18n = R18n::I18n.new('en', DIR)
+    i18n.translations.sort.should == ['English', 'no_LC', 'Русский']
+  end
+  
+  it "should return first locale with locale file" do
+    i18n = R18n::I18n.new(['no_LC', 'ru', 'en'], DIR)
+    i18n.locale.should == R18n::Locale.new('ru')
   end
 
 end
