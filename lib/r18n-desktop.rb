12785b24756c8efca9b5b1a7d6d758404d49cdfd
Andrey A.I. Sitnik <andrey@sitnik.ru>
2008-09-30 22:59:59 +0400

    Initial commit

---
+++ lib/r18n-desktop.rb
@@ -0,0 +1,45 @@
+=begin
+I18n support for desktop application.
+
+Copyright (C) 2008 Andrey “A.I.”" Sitnik <andrey@sitnik.ru>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+require 'rubygems'
+gem 'r18n-core', '=0.1'
+require 'r18n-core'
+
+require 'pathname'
+dir = Pathname(__FILE__).dirname.expand_path + 'r18n-desktop'
+if /cygwin|mingw|win32/ =~ RUBY_PLATFORM
+  require dir + 'win32'
+elsif /java/ =~ RUBY_PLATFORM
+  require dir + 'java'
+else
+  require dir + 'posix'
+end
+
+module R18n
+  class << self
+    # Get user locale from system environment and load I18n object with locale
+    # information and translations from +translations_dir+. If user set locale
+    # +manual+ put it as last argument.
+    def from_env(translations_dir, manual = nil)
+      locales = R18n::I18n.system_locale
+      locales.insert(0, manual) if not manual.nil?
+      self.set I18n.new(locales, translations_dir)
+    end
+  end
+end
---
+++ spec/r18n-desktop_spec.rb
@@ -0,0 +1,28 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe "r18n-desktop" do
+
+  it "should return array of system locales" do
+    locales = R18n::I18n.system_locale
+    locales.class.should == Array
+    locales.should_not be_empty
+    locales.each do |locale|
+      locale.class.should == String
+      locale.should_not be_empty
+    end
+  end
+
+  it "should load I18n from system environment" do
+    i18n = R18n.from_env('')
+    i18n.class.should == R18n::I18n
+    if R18n::Locale != i18n.locale.class
+      i18n.locale.should_not be_empty
+    end
+    
+    i18n = R18n.from_env('', 'en')
+    i18n.locale.should == R18n::Locale.new('en')
+    
+    R18n.get.should == i18n
+  end
+
+end
