8405d976235b38327d099b7826e8371b1176ba8e
Andrey A.I. Sitnik <andrey@sitnik.ru>
2008-12-06 03:14:50 +0300

    Bugfix in specs

--- spec/r18n-desktop_spec.rb
+++ spec/r18n-desktop_spec.rb
@@ -3,20 +3,18 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe "r18n-desktop" do
 
   it "should return array of system locales" do
-    locales = R18n::I18n.system_locale
-    locales.class.should == String
-    locales.should_not be_empty
+    locale = R18n::I18n.system_locale
+    locale.class.should == String
+    locale.should_not be_empty
   end
 
   it "should load I18n from system environment" do
     i18n = R18n.from_env('')
     i18n.class.should == R18n::I18n
-    if R18n::Locale != i18n.locale.class
-      i18n.locale.should_not be_empty
-    end
+    i18n.locale.should_not be_empty if String == i18n.locale.class
     
     i18n = R18n.from_env('', 'en')
-    i18n.locale.should == R18n::Locale.new('en')
+    i18n.locale.should == R18n::Locale.load('en')
     
     R18n.get.should == i18n
   end
