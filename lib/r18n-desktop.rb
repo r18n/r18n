a5a47c629cc9c40190b76f8428c3536d87c6b8be
Andrey A.I. Sitnik <andrey@sitnik.ru>
2008-12-24 03:09:06 +0300

    Trsnaltions dir is optional

--- lib/r18n-desktop.rb
+++ lib/r18n-desktop.rb
@@ -36,7 +36,7 @@ module R18n
     # Get user locale from system environment and load I18n object with locale
     # information and translations from +translations_dir+. If user set locale
     # +manual+ put it as last argument.
-    def from_env(translations_dir, manual = nil)
+    def from_env(translations_dir = nil, manual = nil)
       locales = Array(R18n::I18n.system_locale)
       locales.insert(0, manual) if not manual.nil?
       self.set I18n.new(locales, translations_dir)
