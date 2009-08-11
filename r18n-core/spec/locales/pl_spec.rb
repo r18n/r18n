c89f7fa870aaf96a92e17cb33213b9c7003fda9a
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-11 21:14:29 +1100

    Clean up spec for Polish locale

--- r18n-core/spec/locales/pl_spec.rb
+++ r18n-core/spec/locales/pl_spec.rb
@@ -3,23 +3,23 @@ require File.join(File.dirname(__FILE__), "..", "..", "locales", "pl")
 
 describe R18n::Locales::Pl do
   it "should use Polish pluralization" do
-    ru = R18n::Locale.load 'pl'
-    ru.pluralize(0).should == 0
+    pl = R18n::Locale.load 'pl'
+    pl.pluralize(0).should == 0
     
-    ru.pluralize(1).should == 1
-    ru.pluralize(21).should == 'n'
-    ru.pluralize(101).should == 'n'
+    pl.pluralize(1).should == 1
     
-    ru.pluralize(2).should == 2
-    ru.pluralize(4).should == 2
-    ru.pluralize(22).should == 2
-    ru.pluralize(102).should == 2
+    pl.pluralize(2).should == 2
+    pl.pluralize(4).should == 2
+    pl.pluralize(22).should == 2
+    pl.pluralize(102).should == 2
     
-    ru.pluralize(5).should == 'n'
-    ru.pluralize(11).should == 'n'
-    ru.pluralize(12).should == 'n'
-    ru.pluralize(57).should == 'n'
-    ru.pluralize(111).should == 'n'
-    ru.pluralize(112).should == 'n'
+    pl.pluralize(5).should == 'n'
+    pl.pluralize(11).should == 'n'
+    pl.pluralize(12).should == 'n'
+    pl.pluralize(21).should == 'n'
+    pl.pluralize(57).should == 'n'
+    pl.pluralize(101).should == 'n'
+    pl.pluralize(111).should == 'n'
+    pl.pluralize(112).should == 'n'
   end
 end
