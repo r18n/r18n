6e2cc66fb2d98bdb0c413e336355c1177a623c9a
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-10 13:23:04 +0400

    Bugfix POSIX driver on empty LANG

--- r18n-desktop/lib/r18n-desktop/posix.rb
+++ r18n-desktop/lib/r18n-desktop/posix.rb
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 module R18n
   class I18n
     def self.system_locale
+      return unless ENV['LANG']
       ENV['LANG'].split('.').first
     end
   end
