14f34aa5fe2b4cbdd9244805ce2c909ca2f397c3
Guido De Rosa <job@guidoderosa.net>
2009-11-20 10:35:06 +0000

    merge from git://github.com/ai/r18n.git

--- r18n-core/locales/it.rb
+++ r18n-core/locales/it.rb
@@ -1,10 +1,11 @@
+# encoding: utf-8
 module R18n
   module Locales
     class It < R18n::Locale
       def format_date_full(i18n, date, year = true)
         full = super(i18n, date, year)
         if ' 1' == full[0..1]
-          "1\u00ba" + full[2..-1] 
+          "1º" + full[2..-1]
         else
           full
         end
--- r18n-core/spec/locales/it_spec.rb
+++ r18n-core/spec/locales/it_spec.rb
@@ -1,11 +1,12 @@
+# encoding: utf-8
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 require File.join(File.dirname(__FILE__), '..', '..', 'locales', 'it')
-
+ 
 describe R18n::Locales::It do
   it "should format Italian date" do
     italian = R18n::I18n.new('it')
-    italian.l(Date.parse('2009-07-01'), :full).should ==  "1\u00ba Luglio 2009"
-    italian.l(Date.parse('2009-07-02'), :full).should ==  ' 2 Luglio 2009'
-    italian.l(Date.parse('2009-07-12'), :full).should ==  '12 Luglio 2009'
+    italian.l(Date.parse('2009-07-01'), :full).should == "1º Luglio 2009"
+    italian.l(Date.parse('2009-07-02'), :full).should == ' 2 Luglio 2009'
+    italian.l(Date.parse('2009-07-12'), :full).should == '12 Luglio 2009'
   end
 end
