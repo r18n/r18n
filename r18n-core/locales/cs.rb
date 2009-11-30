4cab36e38815ee5dce0a48c1a451e3c17c1fa137
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-11-30 23:23:34 +0300

    Clean up Czech locale class

--- r18n-core/locales/cs.rb
+++ r18n-core/locales/cs.rb
@@ -2,13 +2,9 @@ module R18n
   module Locales
     class Cs < R18n::Locale
       def pluralize(n)
-        return 0 if n == 0
-        # 0 - 0
-        # 1 - 1
-        # 2..4 - 2
-        # n
-    
         case n
+        when 0
+          0
         when 1
           1
         when 2..4
