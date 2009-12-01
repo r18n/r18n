ff40cc6f12c4e140195c50b49ced0298dd4c5532
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-12-01 22:13:42 +0300

    Fix specs for Ubuntu Rubygems in Ruby 1.9.1

--- r18n-core/spec/spec_helper.rb
+++ r18n-core/spec/spec_helper.rb
@@ -9,3 +9,6 @@ require dir + '../lib/r18n-core'
 DIR = dir + 'translations/general' unless defined? DIR
 TWO = dir + 'translations/two' unless defined? TWO
 EXT = R18n::Loader::YAML.new(dir + 'translations/extension') unless defined? EXT
+
+gem 'maruku'
+gem 'RedCloth'
