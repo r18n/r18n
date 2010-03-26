bb8ba845573e41f159d7fe827c50a62515ff2bb5
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2010-03-26 22:30:45 +0300

    Improve specs

--- r18n-desktop/spec/r18n-desktop_spec.rb
+++ r18n-desktop/spec/r18n-desktop_spec.rb
@@ -4,6 +4,10 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe "r18n-desktop" do
   include R18n::Helpers
 
+  before(:each) do
+    R18n.reset
+  end
+
   it "should return array of system locales" do
     locale = R18n::I18n.system_locale
     locale.class.should == String
@@ -14,11 +18,12 @@ describe "r18n-desktop" do
     R18n.from_env
     r18n.class.should == R18n::I18n
     r18n.locale.should_not be_empty if String == r18n.locale.class
-    R18n.reset
-    
+    R18n.get.should == r18n
+  end    
+
+  it "should load i18n from system environment using specified order" do
     R18n.from_env(nil, 'en')
     r18n.locale.should == R18n::Locale.load('en')
-    
     R18n.get.should == r18n
   end
 
