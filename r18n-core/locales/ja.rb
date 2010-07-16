fc0d5d69dc041e26d74be33df60878039ab76b11
hryk <storage.hn@gmail.com>
2010-07-16 17:18:28 +0900

    Added Japanese locale

---
+++ r18n-core/locales/ja.rb
@@ -0,0 +1,31 @@
+module R18n
+  class Locales::Ja < Locale
+    @@jp_month = %w{一月 二月 三月 四月 五月 六月 七月 八月 九月 十月 十一月 十二月}
+    set :title => 'Japanese',
+        :sublocales => [],
+        
+        :week_start => :sunday,
+        :wday_names => %w{日曜日 月曜日 火曜日 水曜日 木曜日 金曜日 土曜日},
+        :wday_abbrs => %w{日 月 火 水 木 金 土},
+        
+        :month_names => @@jp_month,
+        :month_abbrs => @@jp_month,
+        
+        :date_format => '%Y/%m/%d',
+        :full_format => '%e of %B',
+        :year_format => '_, %Y',
+        
+        :number_decimal => ".",
+        :number_group =>   ","
+    
+    def ordinalize(n)
+      "#{n}番目"
+    end
+    
+    def format_date_full(date, year = true, *params)
+      format = full_format
+      format = year_format.sub('_', format) if year
+      strftime(date, format.sub('%e', ordinalize(date.mday)))
+    end
+  end
+end
