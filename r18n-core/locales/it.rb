4d9a31c7d987fa9a339d5506e78d0bf64c31113e
Viktors Rotanovs <viktors.rotanovs@enormo.com>
2010-08-27 20:35:30 +0400

    Fix Italian locale

--- r18n-core/locales/it.rb
+++ r18n-core/locales/it.rb
@@ -7,7 +7,8 @@ module R18n
                           sabato},
         :wday_abbrs => %w{dom lun mar mer gio ven sab},
         
-        :month_names => %w{gennaio febbraio marzo aprile maggio giugno luglio},
+        :month_names => %w{gennaio febbraio marzo aprile maggio giugno
+                           luglio agosto settembre ottobre novembre dicembre},
         :month_abbrs => %w{gen feb mar apr mag giu lug ago set ott nov dic},
         
         :date_format => '%d/%m/%Y',
