d0f0d4d090ccd0b60ddb57439e7a69fe2f171454
Hans Czajkowski Jørgensen <hans.c.jorgensen@gmail.com>
2010-09-21 18:39:12 +0200

    Added Danish locale

---
+++ r18n-core/locales/da.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+module R18n
+  class Locales::Da < Locale
+    set :title => 'Dansk',
+        
+        :wday_names => %w{søndag mandag tirsdag onsdag torsdag fredag
+                          lørdag},
+        :wday_abbrs => %w{søn man tir ons tor fre lør},
+        
+        :month_names => %w{januar februar marts april Maj juni juli august
+                           september oktober november december},
+        :month_abbrs => %w{jan. feb. mar. apr. maj jun. jul. aug. sep. okt.
+                           nov. dec.},
+        
+        :time_am => 'om formiddagen',
+        :time_pm => 'om eftermiddagen',
+        :date_format => '%d.%m.%Y',
+        :full_format => '%d. %B %Y',
+        
+        :number_decimal => ",",
+        :number_group   => "."
+  end
+end
