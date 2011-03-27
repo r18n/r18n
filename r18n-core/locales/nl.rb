94d3ec27b5e078b3a4811d486e27ead45422b43d
Sander Heilbron <sanderheilbron@gmail.com>
2011-03-27 12:07:33 +0200

    Added the month 'mei' in :month_abbrs

--- r18n-core/locales/nl.rb
+++ r18n-core/locales/nl.rb
@@ -7,7 +7,7 @@ module R18n
         :wday_abbrs => %w{zo ma di wo do vr za},
         
         :month_names => %w{januari februari maart april mei juni juli augustus september oktober november december},
-        :month_abbrs => %w{jan feb mrt apr jun jul aug sep okt nov dec},
+        :month_abbrs => %w{jan feb mrt apr mei jun jul aug sep okt nov dec},
         
         :time_am => '\'s ochtends',
         :time_pm => '\'s middags',
