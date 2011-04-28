75a6a075658d8f1ac224452f5f5bb99dd80d1ee1
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-04-28 14:55:40 +0400

    R18n-Rails: Still using old RSpec and old Rails. But use Bundler to eliminate any troubles.

--- r18n-rails/spec/app/config/environment.rb
+++ r18n-rails/spec/app/config/environment.rb
@@ -1,5 +1,3 @@
-RAILS_GEM_VERSION = '~> 2.3' unless defined? RAILS_GEM_VERSION
-
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
--- r18n-rails/spec/spec_helper.rb
+++ r18n-rails/spec/spec_helper.rb
@@ -1,6 +1,9 @@
-require 'pp'
+require 'rubygems'
 
 ENV['RAILS_ENV'] ||= 'test'
+require 'pp'
 require File.expand_path(File.join(File.dirname(__FILE__), 'app/config/environment'))
+
 require 'spec'
 require 'spec/rails'
+
