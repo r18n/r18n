e310465d2d1d619d9e08cf4aabcf4f5c3f5562ce
Andrey A.I. Sitnik <andrey@sitnik.ru>
2012-03-02 03:15:29 +0400

    Use Ruby 1.8 syntax in Travis Rubinius

--- r18n-rails/spec/app/config/environments/production.rb
+++
@@ -1,9 +0,0 @@
-App::Application.configure do
-  config.cache_classes = true
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
-  config.serve_static_assets = false
-  config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
-end
--- r18n-rails/spec/app/config/environments/test.rb
+++ r18n-rails/spec/app/config/environments/test.rb
@@ -1,10 +1,10 @@
 App::Application.configure do
-  config.cache_classes = true
-  config.whiny_nils = true
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.action_dispatch.show_exceptions = false
-  config.action_controller.allow_forgery_protection    = false
-  config.action_mailer.delivery_method = :test
-  config.active_support.deprecation = :stderr
+  config.cache_classes                              = true
+  config.whiny_nils                                 = true
+  config.consider_all_requests_local                = true
+  config.action_controller.perform_caching          = false
+  config.action_dispatch.show_exceptions            = false
+  config.action_controller.allow_forgery_protection = false
+  config.action_mailer.delivery_method              = :test
+  config.active_support.deprecation                 = :stderr
 end
