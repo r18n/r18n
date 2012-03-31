95a9d9e47a8a666cb1bac6a37b683ea4f2f456a6
David Long <dave@davejlong.com>
2012-03-30 21:35:15 -0400

    Finished porting Swahili translation from svenfuchs/rails-i18n

--- r18n-core/locales/sw.rb
+++ r18n-core/locales/sw.rb
@@ -5,14 +5,22 @@ module R18n
 
 			:week_start => :sunday,
 			:wday_names => %w{Jumapili Jumatatu Jumanne Jumatano Alhamisi Ijumaa Jumamosi},
-			:wday_abbrs => %w{J2 J3 J4 J5 Al Ij J1}
+			:wday_abbrs => %w{J2 J3 J4 J5 Al Ij J1},
 
 			:month_names => %w{"Mwezi wa kwanza" "Mwezi wa pili" "Mwezi wa tatu" "Mwezi wa nne" "Mwezi wa tano" "Mwezi wa sita" "Mwezi wa saba" "Mwezi wa nane" "Mwezi wa tisa" "Mwezi wa kumi" "Mwezi wa kumi na moja" "Mwezi wa kumi na mbili" },
 			:month_abbrs => %W{Jan Feb Mac Apr Mei Jun Jul Ago Sep Okt Nov Des},
 
 			:date_format => '%d-%m-%Y',
-			:full_format => '%e %B %Y',
+			:full_format => '%e %B',
 			:year_format => '_ %Y',
 
 			:numer_decimal => ',',
 			:numer_group => '.'
+
+		def format_date_full(date, year=true, *params)
+			format = full_format
+			format = year_format.sub('_', format) if year
+			strgtime(date, format.sub('%e', date.mday))
+		end
+	end
+end
