29b16c9e3c9b8987cb839dc40dacfd8e993b4af0
Andrey Sitnik <andrey@sitnik.ru>
2015-10-29 05:34:19 +0300

    Fix letters encoding in Vietnamese locale

--- r18n-core/locales/vi.rb
+++ r18n-core/locales/vi.rb
@@ -2,14 +2,13 @@ module R18n
   class Locales::Vi < Locale
     set title: 'Tiếng Việt',
 
-        wday_names: ['Ch? nh?t', 'Th? hai', 'Th? ba', 'Th? t?', 'Th? n?m',
-                     'Th? 6', 'Th? b?y'],
+        wday_names: ['Chủ nhật', 'Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm',
+                     'Thứ Sáu', 'Thứ Bảy'],
         wday_abbrs: %w{CN T2 T3 T4 T5 T6 T7},
 
-        month_names: ['th�ng 1', 'th�ng 2', 'th�ng 3', 'th�ng 4',
-                      'th�ng 5', 'th�ng 6', 'th�ng 7', 'th�ng 8',
-                      'th�ng 9', 'th�ng 10', 'th�ng 11',
-                      'th�ng 12'],
+        month_names: ['tháng 1', 'tháng 2', 'tháng 3', 'tháng 4', 'tháng 5',
+                      'tháng 6', 'tháng 7', 'tháng 8',' tháng 9', 'tháng 10',
+                      'tháng 11', 'tháng 12'],
         month_abbrs: %w{th1 th2 th3 th4 th5 th6 th7 th8 th9 th10 th11 th12},
 
         date_format: '%d/%m/%Y',
