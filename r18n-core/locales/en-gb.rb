4df81afa7d7f17ea6287c435f633b14b173c6ae2
Alexander Popov <alex.wayfer@gmail.com>
2017-12-04 13:24:59 +0300

    Make `date_format` of base `en` locale international
    There is no warranty that base `en` will not be showed for US users with another format.
    
    In Ruby on Rails:
    
    * [en](https://github.com/svenfuchs/rails-i18n/blob/fe07d45/rails/locale/en.yml#L42)
    * [en-US](https://github.com/svenfuchs/rails-i18n/blob/fe07d45/rails/locale/en-US.yml#L42)
    * [en-GB](https://github.com/svenfuchs/rails-i18n/blob/fe07d45/rails/locale/en-GB.yml#L42)

--- r18n-core/locales/en-gb.rb
+++ r18n-core/locales/en-gb.rb
@@ -3,6 +3,8 @@ require File.join(File.dirname(__FILE__), 'en')
 module R18n::Locales
   class EnGb < En
     set title: 'British English',
-        sublocales: %w{en}
+        sublocales: %w{en},
+
+        date_format: '%d/%m/%Y'
   end
 end
