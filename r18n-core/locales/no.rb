e577706e4cc51bd3c9b84ae53af0dd7c03019a12
Alexander Popov <alex.wayfer@gmail.com>
2018-01-16 00:01:00 +0300

    Make `no` locale deprecated
    Please, use `nb` instead.

--- r18n-core/locales/no.rb
+++ r18n-core/locales/no.rb
@@ -1,27 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'nb'
+
 module R18n
   module Locales
-    # Norwegian locale
-    class No < Locale
-      set(
-        title: 'Norsk',
-        sublocales: %w[nb nn en],
-
-        week_start: :monday,
-        wday_names: %w[søndag mandag tirsdag onsdag torsdag fredag lørdag],
-        wday_abbrs: %w[søn man tir ons tor fre lør],
-
-        month_names: %w[januar februar mars april mai juni juli august
-                        september oktober november desember],
-        month_abbrs: %w[jan feb mar apr mai jun jul aug sep okt nov des],
-
-        date_format: '%d.%m.%Y',
-        full_format: '%e. %B %Y',
-
-        number_decimal: ',',
-        number_group:   ' '
-      )
+    # @deprecated Norwegian locale
+    class No < Nb
+      class << self
+        extend Gem::Deprecate
+        deprecate :new, 'R18n::Locales::Nb.new', 2018, 3
+      end
     end
   end
 end
