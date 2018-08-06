9b20439194cf2ef8bee43b334add5e90859ce0f0
Alexander Popov <alex.wayfer@gmail.com>
2018-08-07 01:57:07 +0300

    Move `named_variables` filter from `r18n-rails-api` to `r18n-core`
    Revert f59e910

--- r18n-rails-api/spec/filters_spec.rb
+++ r18n-rails-api/spec/filters_spec.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 describe 'Rails filters' do
-  it 'uses named variables' do
-    i18n = R18n::Translation.new(
-      EN, '', locale: EN, translations: { 'echo' => 'Value is %{value}' }
-    )
-
-    expect(i18n.echo(value: 'R18n')).to eq 'Value is R18n'
-    expect(i18n.echo(value: -5.5)).to   eq 'Value is −5.5'
-    expect(i18n.echo(value: 5000)).to   eq 'Value is 5,000'
-    expect(i18n.echo(value: '<b>')).to  eq 'Value is &lt;b&gt;'
-    expect(i18n.echo).to eq 'Value is %{value}'
-  end
-
   it 'uses old variables syntax' do
     i18n = R18n::Translation.new(
       EN, '', locale: EN, translations: { 'echo' => 'Value is {{value}}' }
