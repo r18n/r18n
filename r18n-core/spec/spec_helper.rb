32eed9a359522eff8f4b2504afba225cb67b2dbf
Alexander Popov <alex.wayfer@gmail.com>
2018-10-21 22:39:07 +0300

    Switch from Travis CI to Cirrus CI

--- r18n-core/spec/spec_helper.rb
+++ r18n-core/spec/spec_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+## https://github.com/stevekinney/pizza/issues/103#issuecomment-136052789
+## https://github.com/docker-library/ruby/issues/45
+Encoding.default_external = 'UTF-8'
+
 require 'pp'
 
 require_relative '../lib/r18n-core'
--- sinatra-r18n/spec/spec_helper.rb
+++ sinatra-r18n/spec/spec_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+## https://github.com/stevekinney/pizza/issues/103#issuecomment-136052789
+## https://github.com/docker-library/ruby/issues/45
+Encoding.default_external = 'UTF-8'
+
 require 'pp'
 
 ENV['RACK_ENV'] = 'test'
