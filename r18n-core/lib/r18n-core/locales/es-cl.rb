59484a19eb307879ab0ef90a6acd54ba1ce94923
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2019-05-09 00:15:48 +0200

    added chile

---
+++ r18n-core/lib/r18n-core/locales/es-cl.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'es'
+
+module R18n
+  module Locales
+    # Chile Spanish locale
+    class EsCL < Es
+      set title: 'Chile Spanish',
+          sublocales: %w[es]
+    end
+  end
+end
