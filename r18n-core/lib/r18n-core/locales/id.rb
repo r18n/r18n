038223dd2be5f3b9b2affeebaec2716c01d7a0db
Mohammad Satrio <wolfaeon@gmail.com>
2019-10-05 11:50:19 +0700

    Improve Indonesian translation

--- r18n-core/lib/r18n-core/locales/id.rb
+++ r18n-core/lib/r18n-core/locales/id.rb
@@ -11,10 +11,12 @@ module R18n
         wday_abbrs: %w[Min Sen Sel Rab Kam Jum Sab],
 
         month_names: %w[Januari Februari Maret April Mei Juni Juli Agustus
-                        September Oktober Nopember Desember],
-        month_abbrs: %w[Jan Feb Mar Apr Mei Jun Jul Agu Sep Okt Nop Des],
+                        September Oktober November Desember],
+        month_abbrs: %w[Jan Feb Mar Apr Mei Jun Jul Agu Sep Okt Nov Des],
 
         date_format: '%d/%m/%Y',
+        time_format: '_ pukul %H.%M',
+        time_with_seconds_format: '_ pukul %H.%M.%S',
 
         number_decimal: ',',
         number_group:   '.'
